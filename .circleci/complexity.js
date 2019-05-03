0b232ebab1250e3d1dcd98ab0d56453e983638d1
Johnny Koo <johnnykoo84@gmail.com>
2019-05-03 13:06:13 +0900

    Resolve conflicts

--- .circleci/complexity.js
+++ .circleci/complexity.js
@@ -2,82 +2,83 @@ const { exec } = require('child_process');
 const https = require('https');
 
 // read results.json and add results on the file.
-let studentInfo = require('../student.json')
-let {theClass, student} = studentInfo
+let studentInfo = require("../student.json");
+let { theClass, student } = studentInfo;
 
 const {
-    sumSquaresTimeComplexity,
-    nthPowerTimeComplexity,
-    rockPaperScissorsTimeComplexity,
-    TimeComplexity
-  } = require('../algo-complexity/algo-complexity');
+  sumSquaresTimeComplexity,
+  nthPowerTimeComplexity,
+  rockPaperScissorsTimeComplexity,
+  TimeComplexity
+} = require("../algo-complexity/algo-complexity");
 
 const result = {
-    'class': theClass,
-    'name': student,
-    'problem': 'time-complexity',
-    'answer': answer,
-    'passing': 0,
-    'failing': 0
-}
+  class: theClass,
+  name: student,
+  problem: "time-complexity",
+  answer: answer,
+  passing: 0,
+  failing: 0
+};
 
 if (sumSquaresTimeComplexity === TimeComplexity.LINEAR) {
-    result.passing ++
+  result.passing++;
 } else {
-    result.failing ++
+  result.failing++;
 }
 
 if (nthPowerTimeComplexity === TimeComplexity.LOGARITHMIC) {
-    result.passing ++
+  result.passing++;
 } else {
-    result.failing ++
+  result.failing++;
 }
 
 if (rockPaperScissorsTimeComplexity === TimeComplexity.EXPONENTIAL) {
-    result.passing ++
+  result.passing++;
 } else {
-    result.failing ++
+  result.failing++;
 }
 
-console.log(result)
-
+console.log(result);
 
 exec('echo "$airtable_api_key"', (err, apikey) => {
-    if (err) {
-        console.log(err)
-        throw new Error('echo command did not work right')
+  if (err) {
+    console.log(err);
+    throw new Error("echo command did not work right");
+  }
+
+  if (apikey === "\n") {
+    throw new Error("There is not the airtable_api_key");
+  }
+
+  const options = {
+    hostname: "api.airtable.com",
+    path: "/v0/app8kEq9wXlsuffDy/Hiring%20Assessments",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: " Bearer " + apikey.trim()
     }
+  };
+  console.log(JSON.stringify(options.headers));
 
-    if (apikey === "\n") {
-        throw new Error('There is not the airtable_api_key')
-    }
-
-    const options = {
-        hostname: 'api.airtable.com',
-        path: '/v0/app8kEq9wXlsuffDy/Hiring%20Assessments',
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json',
-        Authorization: ' Bearer ' + apikey.trim()
-        }
-    };
-    console.log(JSON.stringify(options.headers));
-
-    const req = https.request(options, (res) => {
-        res.on('data', (chunk) => {
-            console.log(chunk.toString());
-        // callback(null, result);
-        });
+  const req = https.request(options, res => {
+    res.on("data", chunk => {
+      console.log(chunk.toString());
+      // callback(null, result);
     });
+  });
 
-    req.on('error', (e) => {
-        console.log(e)
-        throw new Error('data did not send to airtable correctlu')
-        // callback(new Error('failure'));
-    });
-    // send the request
-    req.write(JSON.stringify({
-        'fields': result
-    }));
-    req.end();
-})
+  req.on("error", e => {
+    console.log(e);
+    throw new Error("data did not send to airtable correctlu");
+    // callback(new Error('failure'));
+  });
+  // send the request
+  req.write(
+    JSON.stringify({
+      fields: result
+    })
+  );
+  req.end();
+});
