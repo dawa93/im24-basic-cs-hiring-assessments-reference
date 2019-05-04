5985942ab0fa6d180b4d34166416ab0285a0aad7
Johnny Ilmo Koo <johnnykoo84@gmail.com>
2019-05-04 09:45:21 +0900

    Resolve conflict

--- recursion-print-array/recursion-print-array.js
+++ recursion-print-array/recursion-print-array.js
@@ -1,6 +1,25 @@
 /* CODE GOES HERE
  * Don't forget newlines at the end of all files :) */
 var printArray = function() {
+  function _print(targetArr) {
+    if (targetArr.length === 0) {
+      return;
+    } else {
+      let temp = targetArr[0];
+
+      if (Array.isArray(temp)) {
+        _print(temp);
+        _print(targetArr.slice(1));
+      } else if (temp) {
+        console.log(temp);
+        _print(targetArr.slice(1));
+      } else {
+        _print(targetArr.slice(1));
+      }
+    }
+  }
+
+  _print(arguments[0]);
 };
 
 module.exports = printArray;
